dc9cab2158f8339ca34c92a4a0f304763df412fc
Zachary Wenta <reZach@users.noreply.github.com>
2019-11-12 23:44:24 -0600

    now escapes quotes; also works if you close and open up new tabs

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,45 @@
 import * as vscode from 'vscode';
-const editor = vscode.window.activeTextEditor;
 
 export function activate(context: vscode.ExtensionContext) {	
 	console.log('Congratulations, your extension "listtosql" is now active!');
 
 	let disposable = vscode.commands.registerCommand('listtosql.run', () => {		
-
+		
+		let editor = vscode.window.activeTextEditor;
 		if (typeof editor !== "undefined") {
 
 			const selections: vscode.Selection[] = editor.selections;
 			let text:string;
 			editor.edit(builder => {
-				for (const selection of selections) {
-					
-					text = editor.document.getText(selection);
 
-					if (selection.end.line - selection.anchor.line > 1000){
-						vscode.window.showInformationMessage("More than 1000 rows detected!");
-					}
-
-					text = text.replace(/^(.+)$/gm, "'$1', ");
-					text = text.replace(/\r?\n/gm, "");
-
-					// Remove last bit; & finish
-					text = text.substr(0, text.length - 2);
-					text = "(" + text + ")";
-					
-					builder.replace(selection, text);
-				}
+				if (typeof editor !== "undefined"){
+					for (const selection of selections) {										
+						text = editor.document.getText(selection);
+	
+						// If we didn't select any text
+						if (text.length <= 0){
+							continue;
+						}
+	
+						// SQL inserts max out at 1000 rows
+						if (selection.end.line - selection.anchor.line > 1000){
+							vscode.window.showInformationMessage("More than 1000 rows detected!");
+						}
+	
+						text = text.replace(/'/g, "''");
+						text = text.replace(/^(.+)$/gm, "'$1', ");
+						text = text.replace(/\r?\n/gm, "");
+	
+						// Remove last bit; & finish
+						text = text.substr(0, text.length - 2);
+						text = "(" + text + ")";
+						
+						builder.replace(selection, text);
+					}		
+				}				
 			});
+		} else {
+			vscode.window.showErrorMessage("Could not run command with no editor window open.");
 		}
 	});
 
